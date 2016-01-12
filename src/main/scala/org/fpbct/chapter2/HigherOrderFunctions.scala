ca516d1297b02602c8911dce655ad11902336cd7
Kevin Webber <kevin@kevinwebber.ca>
2016-01-12 13:47:52 -0500

    formatting

--- src/main/scala/org/fpbct/chapter2/HigherOrderFunctions.scala
+++ src/main/scala/org/fpbct/chapter2/HigherOrderFunctions.scala
@@ -37,9 +37,11 @@ object HigherOrderFunctions {
   def descending(x: Int, y: Int): Boolean = if (x < y) true else false
 
   def partial1[A, B, C](a: A, f: (A,B) => C): B => C =
-    (b: B) => f(a, b)
+    b => f(a, b)
 
-  def curry[A, B, C](f: (A, B) => C): A => (B => C) = a => b => f(a, b)
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) =
+    a => b => f(a, b)
 
-  def compose[A, B, C](f: B => C, g: A => B): A => C = a => f(g(a))
+  def compose[A, B, C](f: B => C, g: A => B): A => C =
+    a => f(g(a))
 }
